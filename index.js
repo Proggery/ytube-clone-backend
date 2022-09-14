1607250df1de45a835dac1c7f3131e8c2f818367
Proggery <80548375+Proggery@users.noreply.github.com>
2022-09-14 19:15:07 +0200

    asd

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser"
+const cors = require("cors");
+
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
 
 // Routes
 import authRoutes from "./routes/auth.js";
