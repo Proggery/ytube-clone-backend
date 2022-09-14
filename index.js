8573dde1e439e05ec312a6eab7fc8ac3c73a3b93
Proggery <80548375+Proggery@users.noreply.github.com>
2022-09-14 19:46:16 +0200

    asd

--- index.js
+++ index.js
@@ -20,18 +20,6 @@ import commentRoutes from "./routes/comments.js";
 
 dotenv.config();
 
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested, Content-Type, Accept Authorization"
-  );
-  if (req.method === "OPTIONS") {
-    res.header("Access-Control-Allow-Methods", "POST, PUT, PATCH, GET, DELETE");
-    return res.status(200).json({});
-  }
-  next();
-});
 
 const connect = () => {
   mongoose
